38731124b2f13ad36d71c8272400cd45bbb98576
Анастасия Сахарова <elliot.wrt@gmail.com>
2014-10-19 20:47:51 +0600

    all popups

--- scripts/script.js
+++ scripts/script.js
@@ -1,66 +1,28 @@
 $(document).ready(function(){
-	$('#first-button').on('click', function() {
+	$('.page-button').on('click', function() {
 		$('#overlay').removeClass('hidden');
-		$('#first-popup').removeClass('hidden');
+		$('#' + $(this).data('for')).removeClass('hidden');
+	})
+	$('.popup-button').on('click', function() {
+		$('#overlay').addClass('hidden');
+		$(this).parents('.popup').addClass('hidden');
 	})
 	$('#overlay').on('click', function() {
-		$('#overlay').addClass('hidden');
-		$('#first-popup').addClass('hidden');
-	})
-	$('#first-popup__button').on('click', function() {
-		$('#overlay').addClass('hidden');
-		$('#first-popup').addClass('hidden');
+		$(this).addClass('hidden');
+		$('.popup').addClass('hidden');
 	})
 
-	$('#second-button').on('click', function() {
-		$('#overlay').removeClass('hidden');
-		$('#second-popup').removeClass('hidden');
+	$('.page-button-new').on('click', function() {
+		$('#' + $(this).data('for')).removeClass('hidden')
+			.find('.overlay').removeClass('hidden');
 	})
-	$('#overlay').on('click', function() {
-		$('#overlay').addClass('hidden');
-		$('#second-popup').addClass('hidden');
+	$('.new-popup-button').on('click', function() {
+		$(this).parents('.new-container').addClass('hidden');
 	})
-	$('#second-popup__button').on('click', function() {
-		$('#overlay').addClass('hidden');
-		$('#second-popup').addClass('hidden');
-	})
-
-	$('#third-button').on('click', function() {
-		$('#overlay').removeClass('hidden');
-		$('#third-popup').removeClass('hidden');
-	})
-	$('#overlay').on('click', function() {
-		$('#overlay').addClass('hidden');
-		$('#third-popup').addClass('hidden');
-	})
-	$('#third-popup__button').on('click', function() {
-		$('#overlay').addClass('hidden');
-		$('#third-popup').addClass('hidden');
-	})
-
-	$('#fourth-button').on('click', function() {
-		$('#overlay').removeClass('hidden');
-		$('#fourth-popup').removeClass('hidden');
-	})
-	$('#overlay').on('click', function() {
-		$('#overlay').addClass('hidden');
-		$('#fourth-popup').addClass('hidden');
-	})
-	$('#fourth-popup__button').on('click', function() {
-		$('#overlay').addClass('hidden');
-		$('#fourth-popup').addClass('hidden');
-	})
-
-	$('#fifth-button').on('click', function() {
-		$('#overlay').removeClass('hidden');
-		$('#fifth-popup').removeClass('hidden');
-	})
-	$('#overlay').on('click', function() {
-		$('#overlay').addClass('hidden');
-		$('#fifth-popup').addClass('hidden');
-	})
-	$('#fifth-popup__button').on('click', function() {
-		$('#overlay').addClass('hidden');
-		$('#fifth-popup').addClass('hidden');
+	$('.overlay').on('click', function() {
+		if (!$(this).is(':visible'))
+			return;
+		$(this).addClass('hidden');
+		$('.new-container').addClass('hidden');
 	})
 })
